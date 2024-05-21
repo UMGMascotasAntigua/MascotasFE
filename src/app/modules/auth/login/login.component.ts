9b8aa416558bb02ace6daeecf88569553fa29307
Emilio Hernández <emiliohernandezgt@gmail.com>
2024-05-21 11:20:50 -0600

    se soluciona formulario malo de @Ale20gt

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -8,5 +10,20 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
-onSubmit: any;
+  public form: FormGroup;
+
+  constructor(private _fb: FormBuilder, private toastr: ToastrService){
+    this.form = this._fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    })
+  }
+
+  public doLogin(){
+    if(!this.form.valid){
+      this.toastr.warning("Debe completar todos los campos", "Formulario incompleto", {
+        timeOut: 3500
+      })
+    }
+  }
 }
