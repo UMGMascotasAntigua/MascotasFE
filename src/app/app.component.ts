2cbf9862ad05a9855143986151fdf5f0f9eda593
Emilio Hernández <emiliohernandezgt@gmail.com>
2024-05-25 22:34:01 -0600

    mostrar imágenes de mascotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('navbarCollapse', { static: false }) navbarCollapse!: ElementRef;
+
   title = 'pets';
+  constructor(private viewPort: ViewportScroller, private router: Router){
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0);
+        // this.closeNavbar()
+      }
+    });
+  }
+
+  closeNavbar() {
+    if (this.navbarCollapse) {
+      const navbar = this.navbarCollapse.nativeElement;
+      if (navbar.classList.contains('show')) {
+        navbar.classList.remove('show');
+      }
+    }
+  }
 }
