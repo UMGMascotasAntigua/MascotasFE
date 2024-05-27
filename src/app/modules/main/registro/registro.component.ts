c129c3ce01ae62f5bb3d0d90e21e6baed405a67e
Ale20GT <mlimatujo1@miumg.edu.gt>
2024-05-26 23:36:55 -0600

    Registro

---
+++ src/app/modules/main/registro/registro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class HomeComponent {
+
+}
