0e7c48360b4eef7c70f13f2f47629b632d29c9a6
Emilio Hernández <emiliohernandezgt@gmail.com>
2024-05-31 15:32:18 -0600

    added pdf

--- src/app/modules/main/menu/menu.component.ts
+++ src/app/modules/main/menu/menu.component.ts
@@ -1,24 +1,24 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PetsService } from 'src/app/services/pets.service';
-import {Pet} from '../../../models/Pet';
+import { Pet } from '../../../models/Pet';
 import { environment } from 'src/app/environment/environment';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
 import { IStepperOptions, IWizardStep } from 'ngx-form-wizard';
-import { RegistroComponent } from '../registro/registro.component';
 import { Step1Component } from '../adopt/step1.component';
 import { Step2Component } from '../adopt/step2.component';
 import { Step3Component } from '../adopt/step3.component';
 import { Step4Component } from '../adopt/step4.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit{
+export class MenuComponent implements OnInit {
   public mascotas: Pet[] = [];
   public photoEndpoint: string = `${environment.apiUrl}pet/photo/`;
   public selectedPet: Pet | null = null;
@@ -63,10 +63,10 @@ export class MenuComponent implements OnInit{
   public form: FormGroup;
   public petData: Pet | null = null
   public userInfo: any = null;
-  
+
   constructor(private petsService: PetsService, private toastr: ToastrService, public auth: AuthService,
-    private fb: FormBuilder
-  ){
+    private fb: FormBuilder, private elementRef: ElementRef
+  ) {
     this.form = this.fb.group({
       accept: [false, Validators.requiredTrue],
       terms: [false, Validators.requiredTrue],
@@ -80,7 +80,7 @@ export class MenuComponent implements OnInit{
 
   ngOnInit(): void {
     this.initializeForm()
-    if(this.auth.isAuthenticated()){
+    if (this.auth.isAuthenticated()) {
       this.auth.getUserProfile().subscribe((e) => {
         this.userProfile = e;
         this.setRoleFlags()
@@ -90,7 +90,7 @@ export class MenuComponent implements OnInit{
         });
       })
       this.auth.getUserInfo().subscribe((e) => {
-        if(e.success == true){
+        if (e.success == true) {
           this.userInfo = e.result
         }
       })
@@ -100,20 +100,20 @@ export class MenuComponent implements OnInit{
   }
 
   initializeForm(): void {
-    
+
   }
 
-  getPets(){
+  getPets() {
     this.petsService.getPets()
-    .subscribe((e) => {
-      if(e.success == true){
-        this.mascotas = e.result;
-      }else{
-        this.toastr.error(e.message ?? "Error al cargar los perros", "Carga de datos", {
-          timeOut: 4500
-        });
-      }
-    });
+      .subscribe((e) => {
+        if (e.success == true) {
+          this.mascotas = e.result;
+        } else {
+          this.toastr.error(e.message ?? "Error al cargar los perros", "Carga de datos", {
+            timeOut: 4500
+          });
+        }
+      });
   }
 
   setRoleFlags(): void {
@@ -121,7 +121,7 @@ export class MenuComponent implements OnInit{
     this.isUser = this.userProfile && this.userProfile.Perfil.Descripcion === 'Usuario';
   }
 
-  public cancelAdopt(){
+  public cancelAdopt() {
     this.toastr.warning("Se canceló el proceso de adopción", "Adopción", {
       timeOut: 4500
     });
@@ -129,16 +129,16 @@ export class MenuComponent implements OnInit{
   }
 
 
-  adoption(pet: Pet){
+  adoption(pet: Pet) {
     this.toastr.success("Adopción completa", "Adopción", {
       timeOut: 4500
     })
   }
 
-  addToFavorites(pet: Pet){
+  addToFavorites(pet: Pet) {
     this.petsService.addToFavorites(pet)
       .subscribe((e) => {
-        if(e.success == true){
+        if (e.success == true) {
           this.toastr.success(e.message ?? "Mascota agregada a favoritos", "Favoritos", {
             timeOut: 4500
           })
@@ -154,11 +154,11 @@ export class MenuComponent implements OnInit{
     // }
   }
 
-  removeFromFavorites(pet: any){
+  removeFromFavorites(pet: any) {
 
   }
 
-  public adoptPet(pet: Pet){
+  public adoptPet(pet: Pet) {
     Swal.fire({
       title: `Desea adoptar a ${pet.Nombre_Mascota}?`,
       html: "Teniendo en cuenta la responsabilidad que esto conlleva, y aceptando nuestros términos de servicio y condiciones, además de los <a href='https://shorturl.at/swAAE'>derechos animales</a>",
@@ -171,18 +171,18 @@ export class MenuComponent implements OnInit{
       cancelButtonText: "Cancelar",
       footer: "<a href='' (click)='showTerms()'>Términos de servicio y condiciones</a>",
     })
-    .then((r )=> {
-      if(r.isConfirmed){
+      .then((r) => {
+        if (r.isConfirmed) {
 
-      }
-    })
+        }
+      })
   }
 
-  public showTerms(){
+  public showTerms() {
     alert("xd")
   }
 
-  public deletePet(pet: Pet){
+  public deletePet(pet: Pet) {
     Swal.fire({
       title: 'Desea eliminar la mascota?',
       text: 'Este cambio no podrá ser revertido y se perderá toda la información relacionada',
@@ -193,91 +193,133 @@ export class MenuComponent implements OnInit{
       confirmButtonText: "Si, eliminar",
       cancelButtonText: "Cancelar"
     })
-    .then((r )=> {
-      if(r.isConfirmed){
-        this.petsService.deletePet(pet.Codigo_Mascota)
-        .subscribe((e) => {
-          if(e.success == true){
-            this.toastr.success(e.message ?? "Mascota eliminada con éxito", "Registros", {
-              timeOut: 4500
-            });
-            this.mascotas = [];
-            this.getPets();
-          }else{
-            if(pet.Castraciones.length > 0 && pet.Vacunas_Det.length > 0){
-              this.toastr.error("El registro no se puede borrar, ya que tiene relación con vacunas y/o castraciones.", "Registros", {
-                timeOut: 4500
-              })
-            }else{
-              this.toastr.error(e.message ?? "Error al eliminar la mascota", "Registros", {
-                timeOut: 4500
-              })
-            }
-            
-          }
-        })
-      }
-    })
+      .then((r) => {
+        if (r.isConfirmed) {
+          this.petsService.deletePet(pet.Codigo_Mascota)
+            .subscribe((e) => {
+              if (e.success == true) {
+                this.toastr.success(e.message ?? "Mascota eliminada con éxito", "Registros", {
+                  timeOut: 4500
+                });
+                this.mascotas = [];
+                this.getPets();
+              } else {
+                if (pet.Castraciones.length > 0 && pet.Vacunas_Det.length > 0) {
+                  this.toastr.error("El registro no se puede borrar, ya que tiene relación con vacunas y/o castraciones.", "Registros", {
+                    timeOut: 4500
+                  })
+                } else {
+                  this.toastr.error(e.message ?? "Error al eliminar la mascota", "Registros", {
+                    timeOut: 4500
+                  })
+                }
+
+              }
+            })
+        }
+      })
   }
 
-  public deleteCastration(pet: Pet, index: number){
+  public deleteCastration(pet: Pet, index: number) {
     const castration = pet.Castraciones[index];
     this.petsService.deleteCastration(castration.Codigo_Castracion, castration.Codigo_Mascota)
-    .subscribe((e) => {
-      if(e.success == true){
-        this.toastr.success(e.message ?? "Castración eliminada", "Registros", {
-          timeOut: 3500
-        });
-        this.getPets();
-      }
-    })
+      .subscribe((e) => {
+        if (e.success == true) {
+          this.toastr.success(e.message ?? "Castración eliminada", "Registros", {
+            timeOut: 3500
+          });
+          this.getPets();
+        }
+      })
   }
 
-  public deleteVaccine(pet: Pet, index: number){
+  public deleteVaccine(pet: Pet, index: number) {
     const vaccine = pet.Vacunas_Det[index];
     this.petsService.deleteVaccine(vaccine.Codigo_Mvd)
-    .subscribe((e) => {
-      if(e.success == true){
-        this.toastr.success(e.message ?? "Vacuna eliminada", "Registros", {
-          timeOut: 3500
-        });
-        this.getPets();
-      }
-    })
+      .subscribe((e) => {
+        if (e.success == true) {
+          this.toastr.success(e.message ?? "Vacuna eliminada", "Registros", {
+            timeOut: 3500
+          });
+          this.getPets();
+        }
+      })
   }
 
-  public setPet(pet: Pet){
+  public setPet(pet: Pet) {
     this.petData = pet;
   }
 
-  public finishAdopt(){
+  public finishAdopt() {
     window.location.reload();
   }
 
   onSubmit(): void {
     if (this.form.valid) {
-      if(this.petData != null){
+      if (this.petData != null) {
         this.petsService.adoptPet(this.userInfo, this.petData, this.form.value)
-        .subscribe((e) => {
-          if(e.success == true){
-            Swal.fire({
-              titleText: 'Adopción completada',
-              text: 'Recuerde presentar su identificación al acudir a nuestras instalaciones',
-              icon: "success",
-              confirmButtonText: "Cerrar"
-            });
-            this.getPets();
-          }else{
-            this.toastr.error(e.message ?? "Error al completar la adopción", "Adopciones", {
-              timeOut: 4500
-            })
-          }
-        })
-      }else{
+          .subscribe((e) => {
+            if (e.success == true) {
+              const alert = Swal.fire({
+                titleText: 'Adopción completada',
+                html: 'Recuerde presentar su identificación y su <a>constancia de adopción</a> al acudir a nuestras instalaciones',
+                icon: "success",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Descargar constancia",
+                cancelButtonText: "Cancelar",
+              });
+              alert.then((r) => {
+                if (r.isConfirmed) {
+                  const doc = new jsPDF();
+                  doc.setFontSize(14);
+                  doc.text(`Constancia de Adopción`, 105, 15, { align: 'center' });
+
+                  const fecha = new Date().toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' });
+                  doc.setFontSize(10);
+                  doc.text(`Fecha: ${fecha}`, 15, 25);
+
+                  doc.setFontSize(12);
+                  doc.text(`ID de Cita: ${e.result.Id_Cita}`, 15, 40);
+                  doc.text(`Nombre del Usuario: ${e.result.Nombre_Usuario}`, 15, 50);
+                  doc.text(`Dirección: ${e.result.Direccion}`, 15, 60);
+                  doc.text(`Teléfono: ${e.result.Telefono}`, 15, 70);
+                  doc.text(`Fecha de Creación: ${e.result.Fecha}`, 15, 80);
+                  doc.text(`Fecha de Recolección: ${e.result.Fecha_Recoleccion}`, 15, 90);
+                  doc.text(`Comentarios: ${e.result.Comentarios}`, 15, 100);
+                  doc.save('constancia_adopcion.pdf');
+
+                }
+              })
+
+              this.getPets();
+              this.closeModal()
+            } else {
+              this.toastr.error(e.message ?? "Error al completar la adopción", "Adopciones", {
+                timeOut: 4500
+              })
+            }
+          })
+      } else {
         this.toastr.error("Mascota no seleccionada", "Registros", {
           timeOut: 4500
         })
       }
     }
+  }
+
+  closeModal(): void {
+    // Obtiene el elemento del modal por su ID
+    const modalElement = this.elementRef.nativeElement.querySelector('#adoptModal');
+    // Cierra el modal
+    modalElement?.classList.remove('show');
+    modalElement?.setAttribute('aria-hidden', 'true');
+    modalElement?.setAttribute('style', 'display: none');
+    // Elimina la clase de fondo del modal
+    document.body.classList.remove('modal-open');
+    // Elimina el fondo oscuro detrás del modal
+    const modalBackdrop = document.getElementsByClassName('modal-backdrop')[0];
+    modalBackdrop?.parentNode?.removeChild(modalBackdrop);
   }
 }
